de943e36a2caa46b9261754c80748d9fafb0d993
park-sehyuk <tpgur0523@gmail.com>
2025-08-26 16:03:22 +0900

    login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,9 +1,72 @@
-const LoginPage = () => {
-    return(
-        <div id="LoginPage">
+import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
+const LoginPage = ({ isModal, modalClose }) => {
+  const navigate = useNavigate();
 
+  const ModalRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => setEmail(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+
+  const handleClickGoFind = () => navigate();
+  const handleClickGoSignUp = () => navigate("/signup");
+
+  const modal = () => {
+    if (isModal) {
+      ModalRef.current.style.display = "block";
+    } else {
+      ModalRef.current.style.display = "none";
+    }
+  };
+  useEffect(() => {
+    modal();
+  }, [isModal]);
+
+  return (
+    <div id="LoginPage" ref={ModalRef}>
+      <div className="loginPage">
+        <button className="close" onClick={modalClose}>
+          닫기
+        </button>
+        <h1>로그인</h1>
+        <div className="login-from">
+          <div className="login-from-email">
+            <p className="login-email">이메일</p>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={handleChangeEmail}
+            />
+          </div>
+          <div className="login-from-password">
+            <p className="login-password">비밀번호</p>
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div className="login-from-find">
+            <button className="find" onClick={handleClickGoFind}>
+              비밀번호 찾기
+            </button>
+          </div>
         </div>
-    )
-}
+        <div className="sign-btn">
+          <button className="login">로그인</button>
+          <button className="signup" onClick={handleClickGoSignUp}>
+            회원가입
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/components/pages/SignupPage.jsx
+++ src/components/pages/SignupPage.jsx
@@ -1,9 +1,58 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SignupPage.css";
 const SignupPage = () => {
-    return(
-        <div id="SignupPage">
+  const navigate = useNavigate();
 
-        </div>
-    )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [gender, setGender] = useState("man");
+  const [birthDate, setBirthDate] = useState("");
+
+  const handleChangeName = (e) => setName(e.target.value);
+  const handleChangeEmail = (e) => setEmail(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+  const handleChangePasswordCheck = (e) => setPasswordCheck(e.target.value);
+
+  return (
+    <div id="SignupPage">
+      <div className="sign-name">
+        <p>이름</p>
+        <input type="text" value={name} onChange={handleChangeName} />
+      </div>
+      <div className="sign-email">
+        <p>이메일</p>
+        <input type="text" value={email} />
+        <button className="check">중복확인</button>
+      </div>
+      <div className="sign-password">
+        <p>비밀번호</p>
+        <input type="password" value={password} />
+      </div>
+      <div className="sign-passwordCheck">
+        <p>비밀번호 확인</p>
+        <input type="password" value={passwordCheck} />
+      </div>
+      <div className="sign-nickname">
+        <p>닉네임</p>
+        <input type="text" value={nickName} />
+        <button className="check">중복확인</button>
+      </div>
+      <div className="sign-gender">
+        <p>성별</p>
+        <button className="man">남성</button>
+        <button className="woman">여성</button>
+      </div>
+      <div className="sign-birthdate">
+        <p>생년월일</p>
+        <input type="date" value={birthDate} />
+      </div>
+      <button className="signup">회원가입</button>
+    </div>
+  );
+};
 
 export default SignupPage;
