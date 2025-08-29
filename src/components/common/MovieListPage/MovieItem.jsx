d64605e5f4e1e3dc57504c9fe432db01014f6f0c
kwonbk82 <zkbk4982@gmail.com>
2025-08-29 18:45:14 +0900

    8월 마지막주 작업

--- src/components/common/MovieListPage/MovieItem.jsx
+++ src/components/common/MovieListPage/MovieItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const MovieItem = ({movie}) => {
     return(
         <div className="movie-item">
-            <Link to="/moviedetail/:id">
+            <Link to={`/moviedetail/${movie.id}`}>
                 <img className="img" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="예시"/>
                 <p className="title">{movie.title}</p>
             </Link>
