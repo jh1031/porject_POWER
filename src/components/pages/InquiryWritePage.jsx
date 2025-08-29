bb2b06164d2937168324102dcb0bbbe2f9e1eaab
jian080 <raon10377@gmail.com>
2025-08-29 18:35:56 +0900

    test 2

--- src/components/pages/InquiryWritePage.jsx
+++ src/components/pages/InquiryWritePage.jsx
@@ -78,7 +78,7 @@ const InquiryWritePage = () => {
                         maxLength={MAX_CONTENT}
                     />
                     <div className='counter'>
-                        {content.length}/{MAX_CONTENT} byte!
+                        {content.length}/{MAX_CONTENT} byte
                     </div>
                 </div>
             </div>
@@ -99,4 +99,4 @@ const InquiryWritePage = () => {
     )
 }
 
-export default InquiryWritePage;
+export default InquiryWritePage;git 
