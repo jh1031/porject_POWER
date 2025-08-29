d74a1aefbaa181e4848c977ff84ba8566a90cf78
park-sehyuk <tpgur0523@gmail.com>
2025-08-29 15:50:34 +0900

    로그인/회원가입

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,10 +1,8 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 const LoginPage = ({ isModal, modalClose }) => {
   const navigate = useNavigate();
-
-  const ModalRef = useRef();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -12,58 +10,45 @@ const LoginPage = ({ isModal, modalClose }) => {
   const handleChangeEmail = (e) => setEmail(e.target.value);
   const handleChangePassword = (e) => setPassword(e.target.value);
 
-  const handleClickGoFind = () => navigate();
-  const handleClickGoSignUp = () => navigate("/signup");
-
-  const modal = () => {
-    if (isModal) {
-      ModalRef.current.style.display = "block";
-    } else {
-      ModalRef.current.style.display = "none";
-    }
+  const handleClickGoSignUp = () => {
+    modalClose();
+    navigate("/signup");
   };
-  useEffect(() => {
-    modal();
-  }, [isModal]);
 
   return (
-    <div id="LoginPage" ref={ModalRef}>
+    // isModalActive가 true일 때 "active" 클래스를 추가합니다.
+    <div id="LoginPage" className={isModal ? "active" : ""}>
       <div className="loginPage">
+        {/* 닫기 버튼에는 부모에게서 받은 modalClose 함수를 연결합니다. */}
         <button className="close" onClick={modalClose}>
-          닫기
+          &times;
         </button>
         <h1>로그인</h1>
-        <div className="login-from">
-          <div className="login-from-email">
-            <p className="login-email">이메일</p>
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={handleChangeEmail}
-            />
-          </div>
-          <div className="login-from-password">
-            <p className="login-password">비밀번호</p>
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={handleChangePassword}
-            />
-          </div>
-          <div className="login-from-find">
-            <button className="find" onClick={handleClickGoFind}>
+        <form className="login-form">
+          <input
+            type="text"
+            placeholder="이메일"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={handleChangePassword}
+          />
+          <div className="login-form-find">
+            <button type="button" className="find">
               비밀번호 찾기
             </button>
           </div>
-        </div>
-        <div className="sign-btn">
-          <button className="login">로그인</button>
-          <button className="signup" onClick={handleClickGoSignUp}>
-            회원가입
-          </button>
-        </div>
+          <div className="sign-btn">
+            <button className="login">로그인</button>
+            <button className="signup" onClick={handleClickGoSignUp}>
+              회원가입
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
