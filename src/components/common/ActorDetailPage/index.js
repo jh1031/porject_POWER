b6469d11072e0dafa95d7c9e9eb4c27b31b672fc
park-sehyuk <tpgur0523@gmail.com>
2025-09-03 14:06:51 +0900

    배우 디테일 페이지 완

--- src/components/common/ActorDetailPage/index.js
+++ src/components/common/ActorDetailPage/index.js
@@ -1,5 +1,13 @@
 import ActorInfo from "./ActorInfo";
 import ActorMovieList from "./ActorMovieList";
 import ActorMoiveItem from "./ActorMovieItem";
+import ActorCrewMovieList from "./ActorCrewMovieList";
+import ActorCrewMovieItem from "./ActorCrewMovieItem";
 
-export { ActorInfo, ActorMovieList, ActorMoiveItem };
+export {
+  ActorInfo,
+  ActorMovieList,
+  ActorMoiveItem,
+  ActorCrewMovieList,
+  ActorCrewMovieItem,
+};
