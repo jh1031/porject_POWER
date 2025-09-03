4bf9ac5c434b6f35166865bec80773802dc811c1
kwonbk82 <zkbk4982@gmail.com>
2025-09-03 18:24:50 +0900

    배우띄우기 성공

--- src/components/pages/MovieDetailPage.jsx
+++ src/components/pages/MovieDetailPage.jsx
@@ -15,6 +15,8 @@ const MovieDetailPage = () => {
     const [movie, setMovie] = useState([]);
     const { id } = useParams();
     const [genreMovie, setGenreMovie] = useState([]);
+    const [cast,setCast] = useState([]);
+    const [crew,setCrew] = useState([]);
 
     useEffect(() => {
         fetchMovie();
@@ -23,14 +25,20 @@ const MovieDetailPage = () => {
     const fetchMovie = async () => {
         setIsReady(false);
         try {
-            const res1 = await baseApi.get(`/movie/${id}?language=ko-KR`);
+            const res1 = await baseApi.get(`/movie/${id}`);
             const data = await res1.data;
             const res2 = await baseApi.get(
                 `/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=vote_average.desc&with_genres=${data.genres[0].id}&vote_count.gte=200`
             );
             const genreData = await res2.data.results;
+            const res3 = await baseApi.get(`/movie/${id}/credits`);
+            
+            const castData = await res3.data.cast;
+            const crewData = await res3.data.crew;
             setMovie(data);
             setGenreMovie(genreData)
+            setCast(castData)
+            setCrew(crewData)
         } catch (e) {
             console.error('데이터 로딩 실패 :', e);
         } finally {
@@ -40,6 +48,9 @@ const MovieDetailPage = () => {
     // 중복빼기
     const filteredGenreMovie = genreMovie.filter(item => item.id !== movie.id)
     
+    // 감독찾기
+    const director = crew.filter(item => item.job === "Director")
+
     const handleClickGenre = ()=>{
         nav(`/movielist?genre=${movie.genres[0].id}&page=1`)
     }
@@ -91,7 +102,7 @@ const MovieDetailPage = () => {
                 </div>
                 <div className="md-right">
                     <h1>{movie.title}</h1>
-                    <MovieDetailActorList />
+                    <MovieDetailActorList cast={cast} director={director}/>
                     <div className="right-info">
                         <div>
                             <h4>장르</h4>
@@ -112,7 +123,7 @@ const MovieDetailPage = () => {
             <h2>같은 장르 추천 영화</h2>
             <RecommendGenre movie={filteredGenreMovie} handleClickGenre={handleClickGenre}/>
             <h2>같은 감독의 다른 영화</h2>
-            <RecommendDirector movie={movie} />
+            <RecommendDirector movie={movie} director={director} />
         </div>
     );
 };
