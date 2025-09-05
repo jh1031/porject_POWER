df8ba63876f32917f9a5d49e6521079217bcd5b7
park-sehyuk <tpgur0523@gmail.com>
2025-09-05 18:07:47 +0900

    검색결과페이지

--- src/components/pages/SearchResultPage.jsx
+++ src/components/pages/SearchResultPage.jsx
@@ -3,18 +3,24 @@
 import { useSearchParams } from "react-router-dom";
 import SearchMovieListPage from "./SearchMovieListPage";
 import SearchActorListPage from "./SearchActorListPage";
+import "./SearchResultPage.css";
 
 const SearchResultPage = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
   return (
     <div id="SearchResult">
+      <div className="search-result">
+        <h1>
+          "<span>{query}</span>" 검색 결과입니다.
+        </h1>
+      </div>
       <div className="search-movie">
-        <h2>영화 검색 결과</h2>
+        <h1 className="movie">영화</h1>
         <SearchMovieListPage query={query} />
       </div>
       <div className="search-actor">
-        <h2>인물 검색 결과</h2>
+        <h1 className="actor">인물</h1>
         <SearchActorListPage query={query} />
       </div>
     </div>
