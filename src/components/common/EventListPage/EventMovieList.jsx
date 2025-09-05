a389948340a80533e15202f1f0f8b0b4f27d6de9
jihyeon <jh103177@naver.com>
2025-09-05 18:12:01 +0900

    이벤트 리스트 수정

--- src/components/common/EventListPage/EventMovieList.jsx
+++ src/components/common/EventListPage/EventMovieList.jsx
@@ -3,38 +3,12 @@
 import { useState } from 'react';
 
 const EventMovieList = ({ evMovie, text }) => {
-  const [filter, setFilter] = useState('all');
-
-  const handleFilterChange = (e) => setFilter(e.target.value);
-
-  const filteredEvMovie = evMovie.filter((item) => {
-    const isTextMath = item.movieName.includes(text);
-    const isFilterMath =
-      filter === 'all' ||
-      (filter === 'startFastest' )
-      (filter === 'endFastest')
-
-      return isTextMath && isFilterMath;
-    });
-    // ? [...filteredEvMovie].sort(
-    //     (a, b) => new Date(a.startDate) - new Date(b.startDate)
-    //   )
-    // : filteredEvMovie;
-
-    // 내일 해야할거 시작일 빠른순 이랑 마감일 빠른순 으로 보여지게 할건지 
-    // 데이터 받아오는거 정리
 
 
   return (
 
       <div id="EventMovieList">
-        <div className="Event-filter">
-            <select value={filter} onChange={handleFilterChange}>
-            <option value="all">전체</option>
-            <option value="startFastest">다가오는 이벤트</option>
-            <option value="endFastest"></option>
-          </select>
-        </div>
+
       </div>
   )
 };
--- src/components/pages/EventListPage.jsx
+++ src/components/pages/EventListPage.jsx
@@ -18,8 +18,6 @@ const EventListPage = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  // 나중에 지워
-  // commit 용 나중에 지워~
 
   const fetchData = async () => {
     try {
@@ -34,18 +32,75 @@ const EventListPage = () => {
       setIsReady(true);
     }
   };
+  const thisYear = today.getFullYear();
+  const thisMonth = today.getMonth();
+
+  const firstDay = new Date(thisYear, thisMonth, 1);
+  const lastDay = new Date(thisYear, thisMonth + 1, 1);
+
+  const handleClickPrev = () => {
+    setToday(new Date(thisYear, thisMonth - 1, 1));
+  };
+  const handleClickNext = () => {
+    setToday(new Date(thisYear, thisMonth + 1, 1));
+  };
+
+  const [filter, setFilter] = useState('nowEvent');
+
+  const handleFilterChange = (e) => setFilter(e.target.value);
+
+  const filteredEvMovie = evMovie.filter((item) => {
+    const isTextMath = item.movieName.includes(text);
+    const isFilterMath =
+      filter === 'nowEvent' ||
+      (filter === 'startEvnet' )
+      (filter === 'endEvent')
+
+      return isTextMath && isFilterMath;
+    });
+    // ? [...filteredEvMovie].sort(
+    //     (a, b) => new Date(a.startDate) - new Date(b.startDate)
+    //   )
+    // : filteredEvMovie;
+
+    // 내일 해야할거 시작일 빠른순 이랑 마감일 빠른순 으로 보여지게 할건지 
+    // 데이터 받아오는거 정리
+
   if (!isReady) {
     return <div>데이터 로딩 중,,,</div>;
   }
   return (
     <div id="EventListPage">
-      <div className="Event-search">
-        <input
-          type="text"
-          placeholder="🔎 search"
-          value={text}
-          onChange={handleChangeText}
-        />
+        <div className="date-control">
+            <p className="btnLeft">
+          <button onClick={handleClickPrev}>
+            <span>이전 달</span>
+          </button>
+        </p>
+            <div className="thismonth">{today.toISOString().slice(5,7)}월 이벤트</div>
+             <p className="btnRight">
+          <button onClick={handleClickNext}>
+            <span>다음 달</span>
+          </button>
+        </p>
+        </div>
+      <div className="event-top">
+        <div className="event-filter">
+            <select value={filter} onChange={handleFilterChange}>
+            <option value="nowEvent">진행중인 이벤트</option>
+            <option value="startEvent">다가오는 이벤트</option>
+            <option value="endEvent">끝난 이벤트</option>
+            <option value="all">전체</option>
+          </select>
+        </div>
+        <div className="event-search">
+          <input
+            type="text"
+            placeholder="🔎 search"
+            value={text}
+            onChange={handleChangeText}
+          />
+        </div>
       </div>
       <h3>시사회 이벤트</h3>
       <EventMovieList evMovie={evMovie} text={text} />
